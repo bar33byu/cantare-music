e7ab962b25d7e0bd4be17f54703f6f951a93eb80
Bradley Ross <bar33byu@hotmail.com>
2026-06-04 14:51:10 -0600

    Stabilize media tests in jsdom

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,14 @@
 import "@testing-library/jest-dom";
+
+if (typeof HTMLMediaElement !== "undefined") {
+  Object.defineProperties(HTMLMediaElement.prototype, {
+    load: {
+      configurable: true,
+      value: function load() {},
+    },
+    pause: {
+      configurable: true,
+      value: function pause() {},
+    },
+  });
+}
